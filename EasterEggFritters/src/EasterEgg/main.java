11a9669e8308592e780e2563a227d194fff25761
Jskipper0302 <Yunhang@10.0.0.4>
2016-05-08 04:50:53 +0800

    # WARNING: head commit changed in the meantime
    Yunhang Jiang V5

---
+++ EasterEggFritters/src/EasterEgg/main.java
@@ -0,0 +1,34 @@
+package EasterEgg;
+
+/**
+ * This is for team Fritters EasterEgg project
+ * @author Yunhang
+ */
+public class main {
+	/**
+	 * Here is the main calss to make sure print our team name and members name
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		header();
+		print("Yunhang Jiang,");
+		print("Chenwei qiu,");
+		print("Xufang guo,");
+		print("Tzu-Chien Lu,");
+		print("Isaiah Forward.");
+	}
+	
+	/**
+	 * This is the method to print everyone's name
+	 */
+	public static void print(String theS) {
+		System.out.print(" " + theS);
+	}
+
+	/**
+	 * This is the method to print header information
+	 */
+	public static void header() {
+		System.out.print("We are team Fritters... We are...");
+	}
+}
