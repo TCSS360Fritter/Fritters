422b07ba974aa6e93b49420ef03d9596ee912b1f
Jskipper0302 <Yunhang@10.0.0.4>
2016-05-08 04:26:38 +0800

    Yunhang Jiang V4

--- EasterEgg/src/main.java
+++ EasterEgg/src/main.java
@@ -1,16 +1,33 @@
 package EasterEgg;
 
+/**
+ * This is for team Fritters EasterEgg project
+ * @author Yunhang
+ */
 public class main {
-
+	/**
+	 * Here is the main calss to make sure print our team name and members name
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		print(String theS);
+		header();
+		print("Yunhang Jiang,");
+		print("Chenwei qiu,");
+		print("Xufang guo,");
+		print("Tzu-Chien Lu,");
+		print("Isaiah Forward.");
 	}
 	
+	/**
+	 * This is the method to print everyone's name
+	 */
 	public void print(String theS) {
 		System.out.print(" " + theS);
 	}
 
+	/**
+	 * This is the method to print header information
+	 */
 	public void header() {
 		System.out.println("We are team Fritters... We are...");
 	}
