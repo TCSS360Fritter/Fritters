7fe55048c1bdea26d4ad033e1b0caecda7ee7a99
Jskipper0302 <Yunhang@10.0.0.4>
2016-05-08 04:19:01 +0800

    yunhang jiang V3

--- EasterEgg/src/main.java
+++ EasterEgg/src/main.java
@@ -4,21 +4,14 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
+		print(String theS);
+	}
+	
+	public void print(String theS) {
+		System.out.print(" " + theS);
 	}
 
+	public void header() {
+		System.out.println("We are team Fritters... We are...");
+	}
 }
